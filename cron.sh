2c6a981fa9af0c8f18b6e624f5ef6a64fd79b2e9
Yun KwangSeon <middleyks@hanmail.net>
2025-03-24 13:48:49 +0900

    마비노기 거대한 외침의 뿔피리 수정

--- cron.sh
+++ cron.sh
@@ -10,22 +10,12 @@ SCRIPT_PATH=$( cd "$(dirname "$0")" ; pwd )
 ### Nexon
 
 ## Mabinogi
-# NPC 상점 조회 - 40 x 2,058 = 82,320
-php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli npc-shop-list %EB%A5%98%ED%8A%B8 &
-php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli npc-shop-list %EB%A7%8C%EB%8F%8C%EB%A6%B0 &
-php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli npc-shop-list %ED%95%98%ED%94%84 &
-php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli npc-shop-list %EC%9A%B8%ED%94%84 &
 # 거대한 외침의 뿔피리 내역 조회 - 1,440 x 4 = 5,760
 php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli horn-bugle-world-history &
-# 경매장 등록 상품 - 1,440 x 6 = 8,640
-php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list &
-(sleep 10 && php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list)&
-(sleep 20 && php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list)&
-(sleep 30 && php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list)&
-(sleep 40 && php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list)&
-(sleep 50 && php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction list)&
 # 경매장 거래 내역 조회 - 1,440 x 5 = 7,200
 php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction history &
+# 오래된 데이터 자동 삭제
+php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli delete &
 
 
 ## Mabinogi Heroes
@@ -33,3 +23,5 @@ php "$SCRIPT_PATH"/public/index.php nexon mabinogi cli auction history &
 php "$SCRIPT_PATH"/public/index.php nexon mabinogi-heroes cli ranking hall-of-honor &
 # 실시간 랭킹 정보 조회 - 24 x 2 = 48
 php "$SCRIPT_PATH"/public/index.php nexon mabinogi-heroes cli ranking real-time &
+# 최근 1주간 골드 판매량 정보 조회 - 2
+php "$SCRIPT_PATH"/public/index.php nexon mabinogi-heroes cli marketplace gold-top &
